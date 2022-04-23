d669611c5f50da3ee41003539532ec03e5d32599
Dean <dean.wu@rhf-tech.com>
2022-04-23 19:25:39 +0800

    fix some bug

--- demo-provide/src/main/java/com/demo/provide/DemoProvideApplication.java
+++ demo-provide/src/main/java/com/demo/provide/DemoProvideApplication.java
@@ -1,5 +1,6 @@
 package com.demo.provide;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -16,6 +17,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class DemoProvideApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder().sources(DemoProvideApplication.class).run(args);
+		SpringApplication.run(DemoProvideApplication.class, args);
 	}
 }
