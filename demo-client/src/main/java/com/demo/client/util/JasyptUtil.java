df8a5f7a200d6f867635550edda24731fc81a624
g02117 <loveangelo0217@gmail.com>
2022-12-27 10:19:13 +0800

    add JasyptUtil

---
+++ demo-client/src/main/java/com/demo/client/util/JasyptUtil.java
@@ -0,0 +1,32 @@
+package com.demo.client.util;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+
+public class JasyptUtil {
+    public static String encyptPwd(String password, String value) {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(cryptor(password));
+        String result = encryptor.encrypt(value);
+        return result;
+    }
+
+    public static String decyptPwd(String password, String value) {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setConfig(cryptor(password));
+        String result = encryptor.decrypt(value);
+        return result;
+    }
+
+    public static SimpleStringPBEConfig cryptor(String password) {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        return config;
+    }
+}
--- demo-client/src/test/java/TestSpring.java
+++ demo-client/src/test/java/TestSpring.java
@@ -3,14 +3,24 @@ import com.demo.client.service.PrintService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.function.Consumer;
 
+import static com.demo.client.util.JasyptUtil.decyptPwd;
+import static com.demo.client.util.JasyptUtil.encyptPwd;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DemoClientApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = DemoClientApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestSpring {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encodePwd;
+
+    @Value("${test.db.encode}")
+    private String testStr;
 
     @Autowired
     private PrintService printService;
@@ -26,4 +36,18 @@ public class TestSpring {
     public void getVideo(){
         sayHello.accept("11111");
     }
+
+    @Test
+    public void generatorPwd(){
+        System.out.println(encodePwd);
+        // 加密
+        String encPwd = encyptPwd(encodePwd, "123456");
+        // 解密
+        String decPwd = decyptPwd(encodePwd, encPwd);
+        System.out.println(encPwd);
+        System.out.println(decPwd);
+
+        System.out.println(testStr);
+    }
+
 }
